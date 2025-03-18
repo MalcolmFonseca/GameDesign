68ec2b3e8353c99c8ca3697354e97174820aca6b
Malcolm Fonseca <113556151+MalcolmFonseca@users.noreply.github.com>
2025-03-17 23:12:03 -0400

    Fixed Mid Air Player Movement Overriding Recoil

--- GameDesign/Assets/Player/Player.cs
+++ GameDesign/Assets/Player/Player.cs
@@ -6,8 +6,10 @@ using UnityEngine.Rendering;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rigidbody2d;
+    private BoxCollider2D collider2d;
     private float speed = 1f;
-    Vector2 move;
+    private Vector2 move;
+    [SerializeField] private ContactFilter2D groundFilter;
 
     public InputAction moveAction;
 
@@ -15,6 +17,7 @@ public class Player : MonoBehaviour
     {
         moveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<BoxCollider2D>();
     }
 
     void Update()
@@ -24,13 +27,28 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float xSpeed = rigidbody2d.linearVelocityX + move.x * speed;
-        xSpeed = Mathf.Clamp(xSpeed, -5, 5);
-        rigidbody2d.linearVelocity = new Vector2(xSpeed, rigidbody2d.linearVelocityY);
+        if (Grounded())
+        {
+            float xSpeed = rigidbody2d.linearVelocityX + move.x * speed;
+            xSpeed = Mathf.Clamp(xSpeed, -5, 5);
+            rigidbody2d.linearVelocity = new Vector2(xSpeed, rigidbody2d.linearVelocityY);
+        } else
+        {
+            //IN AIR
+            float xSpeed = rigidbody2d.linearVelocityX + move.x * (speed/10);
+            xSpeed = Mathf.Clamp(xSpeed, -5, 5);
+            rigidbody2d.linearVelocity = new Vector2(xSpeed, rigidbody2d.linearVelocityY);
+        }
     }
 
     public void Shoot(float launchPower, Vector2 direction)
     {
         rigidbody2d.linearVelocity = new Vector2(-direction.x * launchPower, -direction.y * launchPower);
     }
+
+    //check if on ground
+    private bool Grounded()
+    {
+        return rigidbody2d.IsTouching(groundFilter);
+}
 }
